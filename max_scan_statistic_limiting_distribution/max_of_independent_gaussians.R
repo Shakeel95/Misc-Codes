c74eded9f1c43bdbc4d94ad67ee8fc062ad579f5
Shakeel95 <41747714+Shakeel95@users.noreply.github.com>
2020-12-02 16:57:35 +0000

    adds plots for exact pdf

--- max_scan_statistic_limiting_distribution/max_of_independent_gaussians.R
+++ max_scan_statistic_limiting_distribution/max_of_independent_gaussians.R
@@ -1,11 +1,18 @@
-library(foreach)
 
-
-#-------------------------------------------------------------------------
+#============================================================================
 #
 # Exact plots to verify limit distribution of max of independent gaussians
 #
-#-------------------------------------------------------------------------
+#============================================================================
+
+library(foreach)
+
+
+#--------
+#
+# Setup 
+#
+#--------
 
 
 # normalizing constants
@@ -17,30 +24,62 @@ b <- function(n) 1 / sqrt(2*log(n))
 
 # exact & limit CDFs
 
-exact.distr <- function(x,n) pnorm(a(n) + b(n)*x) ** n 
+exact.cdf <- function(x,n) pnorm(a(n) + b(n)*x) ** n 
 
-limit.distr <- function(x) exp(-exp(-x))
-
-
-# grid 
-
-x <- seq(from = -2, to = 3, length.out = 1000)
-
-n.seq <- seq(from = 100, to = 500, by = 100)
-
-n.cols <- colorRampPalette(c("blue", "red"))(5)
+limit.cdf <- function(x) exp(-exp(-x))
 
 
-# CDF plots
+# exact & limit PDFs
 
-plot(x, limit.distr(x), type = "l", ylab = "CDF")
+limit.pdf <- function(x) exp(-x)*exp(-exp(-x))
 
-for (n in n.seq) lines(x, sapply(x, function(j) exact.distr(j,n)), col = n.cols[which(n.seq == n)], type = "l")
+exact.pdf <- function(x,n) n * (pnorm(a(n) + b(n)*x)**(n-1)) * dnorm(a(n) + b(n)*x) * b(n)
 
-legend(-2, .95, 
-       legend = sapply(n.seq, function(i) paste0("n = ", i)), 
+
+# grid
+
+steps <- 3
+
+K <- 1000 
+
+x <- seq(from = -3, to = 6, length.out = 1000)
+
+n.seq <- c(seq(from = 5, to = 10, length.out = steps),1000)
+
+cols <- colorRampPalette(c("blue", "red"))(steps)
+
+
+
+#----------------------------
+#
+# Exact PDF & CDF plots
+#
+#----------------------------
+
+
+# exact CDF v. limit CDF
+
+plot(x, limit.cdf(x), type = "l", lty = 2, ylab = "CDF")
+
+for (n in n.seq) lines(x, exact.cdf(x,n), col = n.cols[which(n.seq == n)], type = "l")
+
+legend(-3, 1, 
+       legend = sapply(n.seq, function(i) paste0("n = ", round(i))), 
        col = n.cols, 
        lty = rep(1,length(n.seq)))
+
+
+# exact PDF v. limit PDF
+
+plot(x, limit.pdf(x), type = "l", lty = 2, ylab = "PDF")
+
+for (n in n.seq) lines(x, exact.pdf(x,n), col = n.cols[which(n.seq == n)])
+
+legend(-3, 0.35, 
+       legend = sapply(n.seq, function(i) paste0("n = ", round(i))), 
+       col = n.cols, 
+       lty = rep(1,length(n.seq)))
+
 
 
 #-------------------------------------------------------------------------
@@ -58,7 +97,7 @@ for (e in 1:length(n.seq)) {
   
   n <- n.seq[e]
   
-  max.sim <- foreach(i=1:1000, .combine = c) %do% (max(rnorm(n)) - a(n)) / b(n)
+  max.sim <- foreach(i=1:K, .combine = c) %do% (max(rnorm(n)) - a(n)) / b(n)
   
   CDFs[[e]] <- ecdf(max.sim)
   
@@ -68,11 +107,11 @@ for (e in 1:length(n.seq)) {
 
 # plot limit distribution and ecdfs
 
-plot(x, limit.distr(x), type = "l")
+plot(x, limit.cdf(x), type = "l", lty = 2)
 
 for (e in 1:length(n.seq)) lines(x, CDFs[[e]](x), col = n.cols[e])
 
-legend(-2, .95, 
-       legend = sapply(n.seq, function(i) paste0("n = ", i)), 
+legend(-3, 1, 
+       legend = sapply(n.seq, function(i) paste0("n = ", round(i))), 
        col = n.cols, 
        lty = rep(1,length(n.seq)))
